0eb1d230e40febdcf58a66d8fb4f855e59734973
JehanRodrigo <jehanrodrigo31@gmail.com>
2024-09-11 22:46:11 +0530

    Created Pipe2

--- pipe1.py
+++ pipe1.py
@@ -1,6 +1,7 @@
 from transformers import pipeline
 
 classifier = pipeline("sentiment-analysis", model="distilbert-base-uncased-finetuned-sst-2-english")
+# model="" Not mandatory, but it is recommended to specify the model to use. If not specified, the default model is used.
 
 result = classifier("I've been waiting for a HuggingFace course my whole life.")
 
